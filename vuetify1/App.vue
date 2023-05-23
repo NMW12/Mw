0af884a7be2b96e22702a25f5969855c54b47c0e
NMW12 <134368372+NMW12@users.noreply.github.com>
2023-05-23 16:23:44 +0900

    Add files via upload

---
+++ vuetify1/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+     <v-content>  
+      <Nav></Nav>
+    <router-view></router-view>
+   </v-content>
+  </v-app>
+</template>
+
+<script>
+import Nav from '@/components/Navbar.vue'
+export default {
+  name: 'App',
+
+  data: () => ({
+    Nav
+  }),
+};
+</script>
